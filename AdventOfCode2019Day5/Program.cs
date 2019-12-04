5c287fdda2ceb91023adcfdf58744d36ac9947c9
Stefan <stevetv90@gmail.com>
2019-12-04 22:09:01 +0100

    Day5 created

---
+++ AdventOfCode2019Day5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdventOfCode2019Day5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Part1();
+        }
+
+        static void Part1()
+        {
+            List<int> input = new List<int>() { 1 };
+
+            Console.WriteLine("Hello World!");
+        }
+
+        static void Part2()
+        {
+            List<int> input = new List<int>() { 1 };
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
