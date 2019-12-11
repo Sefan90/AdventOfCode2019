ded4d235744e0bcced2a392098e114ce2cc147b7
Stefan <stevetv90@gmail.com>
2019-12-11 22:33:39 +0100

    Intcode computer updated

--- AdventOfCode2019Day7/Day7Part1.py
+++ AdventOfCode2019Day7/Day7Part1.py
@@ -7,119 +7,73 @@ def GetReverseIntArray(number):
 		result = [0]
 	return result
 
+def modeChecker(input,opcode,*args):
+	item_list = GetReverseIntArray(input[opcode])
+	if len(args) == 0:
+		return item_list[0]
+	if len(args) == 1:
+		if len(item_list) > 1+args[0] and item_list[1+args[0]] == 1:
+			return input[opcode+args[0]]
+		return input[input[opcode+args[0]]]		
+	else:
+		if len(item_list) > 1+args[0] and item_list[1+args[0]] == 1:
+			input[opcode+args[0]] = args[1]
+		else:
+			input[input[opcode+args[0]]] = args[1]	
+
 def IntcodeComputer(input,startInput):
 	startInputCounter = 0
 	output = 0
 	breaks = False
 	i = 0
 	while i < len(input):
-		PosModeA = False
-		PosModeB = False
-		PosModeC = False
-		input1 = 0
-		input2 = 0
-
-		if i+2 < len(input): #För att inte få out of range på input
-			#print(" I:"+str(i)+" OP:"+str(input[i])+" In1:"+str(input[input[i + 1]])+" In2:"+str(input[i + 2])+" In3:"+str(input[i + 3]))
-			input2 = input[i + 2]
-		if i+1 < len(input):
-			input1 = input[i + 1]
-			
-		item_list = GetReverseIntArray(input[i])
-		if (len(item_list) > 1):
-			if (len(item_list) == 2 and item_list[1] == 9 and item_list[0] == 9):
-				breaks = True
-				break
-
-			#if (len(item_list) >= 2 and item_list[1] != 0):
-			#	breaks = True
-			#	print(item_list)
-		#	break
-			
-			if(len(item_list) >= 3 and item_list[2] == 1):
-				PosModeC = True
-			
-			if (len(item_list) >= 4 and item_list[3] == 1):
-				PosModeB = True
-			
-			if (len(item_list) >= 5 and item_list[4] == 1):
-				PosModeA = True
-		#print(item_list)
-		if (PosModeB == False and item_list[0] != 3 and item_list[0] != 4):
-			input2 = input[input2]
-		if (PosModeC == False and item_list[0] != 3 and item_list[0] != 4):
-			input1 = input[input1]
-		#print(input1)
-		#print(input2)
-
-		if (item_list[0] == 1):
-			if (PosModeA == False):
-				input[input[i + 3]] = input1 + input2
-			else:
-				input[i + 3] = input1 + input2
+		if (modeChecker(input,i) == 1):
+			modeChecker(input,i,3,modeChecker(input,i,1) + modeChecker(input,i,2))
 			i += 4
 		
-		elif (item_list[0] == 2):
-			if (PosModeA == False):
-				input[input[i + 3]] = input1 * input2
-			else:
-				input[i + 3] = input1 * input2
+		elif (modeChecker(input,i) == 2):
+			modeChecker(input,i,3,modeChecker(input,i,1) * modeChecker(input,i,2))
 			i += 4
 		
-		elif (item_list[0] == 3):
-			input[input[i + 1]] = startInput[startInputCounter]
+		elif (modeChecker(input,i) == 3):
+			modeChecker(input,i,1,startInput[startInputCounter])
 			startInputCounter += 1
 			i += 2
 		
-		elif (item_list[0] == 4):
+		elif (modeChecker(input,i) == 4):
 			#print(input[input[i + 1]])
-			output = input[input[i + 1]]
-			startInput.append(input[input[i + 1]])
+			output = modeChecker(input,i,1)
+			startInput.append(modeChecker(input,i,1))
 			i += 2
 		
-		elif (item_list[0] == 5):
-			if input1 != 0:
-				i = input2
+		elif (modeChecker(input,i) == 5):
+			if modeChecker(input,i,1) != 0:
+				i = modeChecker(input,i,2)
 			else:
 				i += 3
 
-		elif (item_list[0] == 6):
-			if input1 == 0:
-				i = input2
+		elif (modeChecker(input,i) == 6):
+			if modeChecker(input,i,1) == 0:
+				i = modeChecker(input,i,2)
 			else:
 				i += 3
 
-		elif (item_list[0] == 7):
-			if input1 < input2:
-				if (PosModeA == False):
-					input[input[i+3]] = 1
-				else:
-					input[i+3] = 1
+		elif (modeChecker(input,i) == 7):
+			if modeChecker(input,i,1) < modeChecker(input,i,2):
+				modeChecker(input,i,3,1)
 			else:
-				if (PosModeA == False):
-					input[input[i+3]] = 0
-				else:
-					input[i+3] = 0
+				modeChecker(input,i,3,0)
 			i += 4
 
-		elif (item_list[0] == 8):
-			if input1 == input2:
-				if (PosModeA == False):
-					input[input[i+3]] = 1
-				else:
-					input[i+3] = 1
+		elif (modeChecker(input,i) == 8):
+			if modeChecker(input,i,1) == modeChecker(input,i,2):
+				modeChecker(input,i,3,1)
 			else:
-				if (PosModeA == False):
-					input[input[i+3]] = 0
-				else:
-					input[i+3] = 0
+				modeChecker(input,i,3,0)
 			i += 4
-
 		else:
-			breaks = True
-			break
+			break;
 	return breaks, output
-	#print(input[0])
 
 def PhaseSender():
 	output = 0
@@ -130,22 +84,21 @@ def PhaseSender():
 					for e in range(5,10):
 						if a not in [b,c,d,e] and b not in [c,d,e] and c not in [d,e] and d not in [e]:
 							breaks = False
-							phases = [a,b,c,d,e]
+							phases = [9,7,8,5,6] #[a,b,c,d,e]
 							startInput = 0
-							inputlist = [3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5]
+							#inputlist = [3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5]
+							inputlist = [3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10]
 							#inputlist = [3,8,1001,8,10,8,105,1,0,0,21,42,55,64,85,98,179,260,341,422,99999,3,9,101,2,9,9,102,5,9,9,1001,9,2,9,1002,9,5,9,4,9,99,3,9,1001,9,5,9,1002,9,4,9,4,9,99,3,9,101,3,9,9,4,9,99,3,9,1002,9,4,9,101,3,9,9,102,5,9,9,101,4,9,9,4,9,99,3,9,1002,9,3,9,1001,9,3,9,4,9,99,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,101,1,9,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,99,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,99,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,101,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,99,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,99,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,99]
-							while True:	
-								for phase in phases:
-									breaks = False
-									print(phase)
-									breaks, startInput = IntcodeComputer(inputlist,[phase,startInput])
-									if breaks:
-										break
+							#while True:	
+							for phase in phases:
+								breaks = False
+								print(phase)
+								breaks, startInput = IntcodeComputer(inputlist,[phase,startInput])
 								if output < startInput:
 									output = startInput
 									print(output)
-								if breaks:
-									break
+								#if breaks:
+								#	break
 	print(output)
 
 PhaseSender();
