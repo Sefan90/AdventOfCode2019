c8c46c9724d328672813ca6436735de3031413b5
Stefan <stevetv90@gmail.com>
2019-12-18 07:17:27 +0100

    Update Day16Part1.py

--- AdventOfCode2019Day16/Day16Part1.py
+++ AdventOfCode2019Day16/Day16Part1.py
@@ -15,35 +15,35 @@ def MakePattern(inputList):
 		for b in range(len(basePattern)):
 			bigPattern.insert(i*b,basePattern[b])
 		listPattern.append(bigPattern)
-		print(listPattern[0])
 	return listPattern
 
 def Part1():
 	#inputList = 59791871295565763701016897619826042828489762561088671462844257824181773959378451545496856546977738269316476252007337723213764111739273853838263490797537518598068506295920453784323102711076199873965167380615581655722603274071905196479183784242751952907811639233611953974790911995969892452680719302157414006993581489851373437232026983879051072177169134936382717591977532100847960279215345839529957631823999672462823375150436036034669895698554251454360619461187935247975515899240563842707592332912229870540467459067349550810656761293464130493621641378182308112022182608407992098591711589507803865093164025433086372658152474941776320203179747991102193608
 	#splitedList = GetIntArray(inputList)
-	splitedList = [0,3,0,3,6,7,3,2,5,7,7,2,1,2,9,4,4,0,6,3,4,9,1,5,6,5,4,7,4,6,6,4]
+	#splitedList = [0,3,0,3,6,7,3,2,5,7,7,2,1,2,9,4,4,0,6,3,4,9,1,5,6,5,4,7,4,6,6,4]
+	splitedList =[1,2,3,4,5,6,7,8]
 	tempList = []
-	for i in range(10):
-		tempList = tempList + splitedList
+	#for i in range(10):
+	#	tempList = tempList + splitedList
 	splitedList = tempList
 	patternList = MakePattern(splitedList)
 	for x in range(100):
 		newList= []
-		#basePattern = [0,1,0,-1]
-		#bigPattern = [1,0,-1,0]
+		basePattern = [0,1,0,-1]
+		bigPattern = [1,0,-1,0]
 		for i in range(1,len(splitedList)+1):
-			#if i > 1:
-			#	for b in range(len(basePattern)):
-					#print("t"+str(i*b))
-			#		bigPattern.insert(i*b,basePattern[b])
-			#print(bigPattern)
+			if i > 1:
+				for b in range(len(basePattern)):
+					print("t"+str(i*b))
+					bigPattern.insert(i*b,basePattern[b])
+			print(bigPattern)
 			output = 0
 			bp = 0
 			for item in splitedList:
-				output += item*patternList[i-1][bp]
+				output += item*bigPattern[bp]
 				#print(output)
 				bp += 1
-				bp = bp%len(patternList)
+				bp = bp%len(bigPattern)
 			newList.append(abs(output)%10)
 		splitedList = newList
 
