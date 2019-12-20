d599f3434d91756d1d1bf08e07e6826d51897005
Stefan <stevetv90@gmail.com>
2019-12-20 21:21:52 +0100

    Update Day10Part1.py

--- AdventOfCode2019Day10/Day10Part1.py
+++ AdventOfCode2019Day10/Day10Part1.py
@@ -7,19 +7,46 @@ def FindAsteroids(cordlist,cord):
 	remove = []
 	for c in cordlist:
 		for cc in cordlist:
-			if (c == [0,0] and c not in remove):
+			if c == [0,0] and c not in remove:
 				remove.append(c)
+				print("1 "+str(c)+" "+str(cc))
 				break
-			for i in range(1,30):
-				if (c[0] == cc[0]*i and c[1] == cc[1]*i and c not in remove and c != cc):
-					remove.append(c)
-					break
-				if c[0] != 0 and cc[0] != 0 and c[1] != 0 and cc[1] != 0 and c[0]/cc[0] == c[0]//cc[0] and c[1]/cc[1] == c[1]//cc[1] and c not in remove and c != cc:
-					remove.append(c)
-					break
+			elif c != cc and c != [0,0] and cc != [0,0] and c[0] == 0 and cc[0] == 0 and ((c[1] > 0 and cc[1] > 0 and c[1] > cc[1]) or (c[1] < 0 and cc[1] < 0 and c[1] < cc[1])) not in remove and c != cc:
+				remove.append(c)
+				print("2 "+str(c)+" "+str(cc))
+				break
+			elif c != cc and c != [0,0]and cc != [0,0] and c[1] == 0 and cc[1] == 0 and ((c[0] > 0 and cc[0] > 0 and c[0] > cc[0]) or (c[0] < 0 and cc[0] < 0 and c[0] < cc[0])) not in remove and c != cc:
+				remove.append(c)
+				print("3 "+str(c)+" "+str(cc))
+				break
+			elif c[0] > 1 and cc[0] > 1 and c[1] > 1 and cc[1] > 1 and c[0]/cc[0] == c[0]//cc[0] and c[1]/cc[1] == c[1]//cc[1] and c[0]//cc[0] == c[1]//cc[1] and c not in remove and c != cc:
+				remove.append(c)
+				print("4 "+str(c)+" "+str(cc))
+				print(str(c[1]/cc[1])+" "+str(c[1]//cc[1]))
+				break
+			elif c[0] < -1 and cc[0] > 1 and c[1] < -1 and cc[1] > 1 and c[0]/cc[0] == c[0]//cc[0] and c[1]/cc[1] == c[1]//cc[1] and c[0]//cc[0] == c[1]//cc[1] and c not in remove and c != cc:
+				remove.append(c)
+				print("5 "+str(c)+" "+str(cc))
+				print(str(c[1]/cc[1])+" "+str(c[1]//cc[1]))
+				break
+			elif c[0] < -1 and cc[0] < -1 and c[1] < -1 and cc[1] < -1 and c[0]/cc[0] == c[0]//cc[0] and c[1]/cc[1] == c[1]//cc[1] and c[0]//cc[0] == c[1]//cc[1] and c not in remove and c != cc:
+				remove.append(c)
+				print("6 "+str(c)+" "+str(cc))
+				print(str(c[1]/cc[1])+" "+str(c[1]//cc[1]))
+				break
+			elif c[0] > 1 and cc[0] < -1 and c[1] > 1 and cc[1] < -1 and c[0]/cc[0] == c[0]//cc[0] and c[1]/cc[1] == c[1]//cc[1] and c[0]//cc[0] == c[1]//cc[1] and c not in remove and c != cc:
+				remove.append(c)
+				print("7 "+str(c)+" "+str(cc))
+				print(str(c[1]/cc[1])+" "+str(c[1]//cc[1]))
+				break
+				#for i in range(1,30):
+				#if c[0] == cc[0]*i and c[1] == cc[1]*i and c not in remove and c != cc:
+				#remove.append(c)
+				#break
+	print(remove)
 	for r in remove:
 		cordlist.remove(r)
-	#print(cordlist)
+	print(cordlist)
 	#print(len(cords))
 	return len(cordlist)
 
@@ -27,8 +54,8 @@ def Part1():
 	cords = []
 	mostAsteroids = 0
 	bestCords = []
-	#map =[".#..#",".....","#####","....#","...##"]
-	map = ["......#.#.","#..#.#....","..#######.",".#.#.###..",".#..#.....","..#....#.#","#..#....#.",".##.#..###","##...#..#.",".#....####"]
+	map =[".#..#",".....","#####","....#","...##"]
+	#map = ["......#.#.","#..#.#....","..#######.",".#.#.###..",".#..#.....","..#....#.#","#..#....#.",".##.#..###","##...#..#.",".#....####"]
 	#map = ["#.#...#.#.",".###....#.",".#....#...","##.#.#.#.#","....#.#.#.",".##..###.#","..#...##..","..##....##","......#...",".####.###."]
 	#map = [".#..#..###","####.###.#","....###.#.","..###.##.#","##.##.#.#.","....###..#","..#.#..#.#","#..#.#.###",".##...##.#",".....#.#.."]
 	#map = [".#......##.#..#.......#####...#..","...#.....##......###....#.##.....","..#...#....#....#............###.",".....#......#.##......#.#..###.#.","#.#..........##.#.#...#.##.#.#.#.","..#.##.#...#.......#..##.......##","..#....#.....#..##.#..####.#.....","#.............#..#.........#.#...","........#.##..#..#..#.#.....#.#..",".........#...#..##......###.....#","##.#.###..#..#.#.....#.........#.",".#.###.##..##......#####..#..##..",".........#.......#.#......#......","..#...#...#...#.#....###.#.......","#..#.#....#...#.......#..#.#.##..","#.....##...#.###..#..#......#..##","...........#...#......#..#....#..","#.#.#......#....#..#.....##....##","..###...#.#.##..#...#.....#...#.#",".......#..##.#..#.............##.","..###........##.#................","###.#..#...#......###.#........#.",".......#....#.#.#..#..#....#..#..",".#...#..#...#......#....#.#..#...","#.#.........#.....#....#.#.#.....",".#....#......##.##....#........#.","....#..#..#...#..##.#.#......#.#.","..###.##.#.....#....#.#......#...","#.##...#............#..#.....#..#",".#....##....##...#......#........","...#...##...#.......#....##.#....",".#....#.#...#.#...##....#..##.#.#",".#.#....##.......#.....##.##.#.##"]
@@ -40,8 +67,9 @@ def Part1():
 	for c in cords:
 		print(c)
 		cTemp = c.copy()
+		ltemp = cords.copy()
 		bestCords.append([cTemp,0])
-		temp = FindAsteroids(cords.copy(),c)
+		temp = FindAsteroids(ltemp,cTemp)
 		bestCords[len(bestCords)-1][1]=temp
 		if mostAsteroids < temp:
 			mostAsteroids = temp
