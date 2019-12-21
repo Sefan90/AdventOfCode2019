4881539b6ba4d83521239d03f8f98f802639df35
Stefan <stevetv90@gmail.com>
2019-12-21 21:56:24 +0100

    Update Day10Part1.py
    Part 1 and 2 done

--- AdventOfCode2019Day10/Day10Part1.py
+++ AdventOfCode2019Day10/Day10Part1.py
@@ -1,4 +1,7 @@
+from functools import reduce
 import math
+import operator
+
 def FindAsteroids(cordlist,cord):
 	tempList = []
 	for c in cordlist:
@@ -13,12 +16,9 @@ def FindAsteroids(cordlist,cord):
 			remove.append([c,temp])
 			for r in range(len(remove)-1):
 				if remove[r][1] == temp:
-					if remove[r][0] > c:
-						remove[r][0] == c
+					if math.sqrt(remove[r][0][0]**2+remove[r][0][1]**2) > math.sqrt(c[0]**2+c[1]**2):
+						remove[r][0] = c
 					remove.pop()
-	#print(remove)
-	#print(cordlist)
-	#print(len(cords))
 	
 	#return len(remove) #Return for Part1
 	return remove
@@ -27,6 +27,7 @@ def Part1():
 	cords = []
 	mostAsteroids = 0
 	bestCords = []
+	testcord = []
 	best = [[0,0],0]
 	#map =[".#..#",".....","#####","....#","...##"]
 	#map = ["......#.#.","#..#.#....","..#######.",".#.#.###..",".#..#.....","..#....#.#","#..#....#.",".##.#..###","##...#..#.",".#....####"]
@@ -37,14 +38,16 @@ def Part1():
 	for x in range(len(map)):
 		for y in range(len(map[x])):
 			if map[x][y] == "#":
-				cords.append([x,y])
-	#print(cords)
+				cords.append([y,x])
+				testcord.append([y,x])
+	counter = 0
 	for c in cords:
-		print(c)
+		#print(c)
 		cTemp = c.copy()
 		ltemp = cords.copy()
-		bestCords.append([cTemp,0])
-		temp = FindAsteroids(ltemp,cTemp)
+		bestCords.append([testcord[counter],0])
+		counter += 1
+		temp = FindAsteroids(ltemp.copy(),cTemp.copy())
 		bestCords[len(bestCords)-1][1]=temp
 		if mostAsteroids < temp:
 			mostAsteroids = temp
@@ -56,43 +59,29 @@ def Part1():
 			print(b)
 	print(best)
 	print(mostAsteroids)
+	print(testcord)
 
 def Part2():
-	best = [0, 6] #Output from Part1
+	best = [29,28]
+	best2 = (29,28) #Output from Part1
 	cords = []
 	degrList = []
-	currentdegree = 90
-	currentlength = 0
-	lastdestroyed = []
-	#map =[".#....#####...#..","##...##.#####..##","##...#...#.#####.","..#.....X...###..","..#.#.....#....##"]
-	map = [".#......##.#..#.......#####...#..","...#.....##......###....#.##.....","..#...#....#....#............###.",".....#......#.##......#.#..###.#.","#.#..........##.#.#...#.##.#.#.#.","..#.##.#...#.......#..##.......##","..#....#.....#..##.#..####.#.....","#.............#..#.........#.#...","........#.##..#..#..#.#.....#.#..",".........#...#..##......###.....#","##.#.###..#..#.#.....#.........#.",".#.###.##..##......#####..#..##..",".........#.......#.#......#......","..#...#...#...#.#....###.#.......","#..#.#....#...#.......#..#.#.##..","#.....##...#.###..#..#......#..##","...........#...#......#..#....#..","#.#.#......#....#..#.....##....##","..###...#.#.##..#...#.....#...#.#",".......#..##.#..#.............##.","..###........##.#................","###.#..#...#......###.#........#.",".......#....#.#.#..#..#....#..#..",".#...#..#...#......#....#.#..#...","#.#.........#.....#....#.#.#.....",".#....#......##.##....#........#.","....#..#..#...#..##.#.#......#.#.","..###.##.#.....#....#.#......#...","#.##...#............#..#.....#..#",".#....##....##...#......#........","...#...##...#.......#....##.#....",".#....#.#...#.#...##....#..##.#.#",".#.#....##.......#.....##.##.#.##"]
-	for x in range(len(map)):
-		for y in range(len(map[x])):
-			if map[x][y] == "#":
-				cords.append([x,y])
+	sortList = []
+	degrSet = []
+	inputMap = [".#......##.#..#.......#####...#..","...#.....##......###....#.##.....","..#...#....#....#............###.",".....#......#.##......#.#..###.#.","#.#..........##.#.#...#.##.#.#.#.","..#.##.#...#.......#..##.......##","..#....#.....#..##.#..####.#.....","#.............#..#.........#.#...","........#.##..#..#..#.#.....#.#..",".........#...#..##......###.....#","##.#.###..#..#.#.....#.........#.",".#.###.##..##......#####..#..##..",".........#.......#.#......#......","..#...#...#...#.#....###.#.......","#..#.#....#...#.......#..#.#.##..","#.....##...#.###..#..#......#..##","...........#...#......#..#....#..","#.#.#......#....#..#.....##....##","..###...#.#.##..#...#.....#...#.#",".......#..##.#..#.............##.","..###........##.#................","###.#..#...#......###.#........#.",".......#....#.#.#..#..#....#..#..",".#...#..#...#......#....#.#..#...","#.#.........#.....#....#.#.#.....",".#....#......##.##....#........#.","....#..#..#...#..##.#.#......#.#.","..###.##.#.....#....#.#......#...","#.##...#............#..#.....#..#",".#....##....##...#......#........","...#...##...#.......#....##.#....",".#....#.#...#.#...##....#..##.#.#",".#.#....##.......#.....##.##.#.##"]
+	for x in range(len(inputMap)):
+		for y in range(len(inputMap[x])):
+			if inputMap[x][y] == "#":
+				cords.append([y,x])
 	degrList = FindAsteroids(cords,best)
-	degrList.sort(key = lambda x: x[1], reverse = True)
-	print(degrList)
-	for i in range(200):
-		for x in range(len(degrList)):
-			if currentdegree <= 0:
-				currentdegree = 180
-			#print(cordlist[x])
-			if degrList[x][1] > currentdegree:
-				continue
-			else:
-				print(str(i)+" "+str(currentdegree))
-				currentdegree = degrList[x][1]
-				lastdestroyed = degrList[x]
-				degrList.remove(degrList[x])
-				break
-	print((lastdestroyed[0][0]+best[0])*100+lastdestroyed[0][1]+best[1])
-
-
-	#print(degrList)
-	#print(math.degrees(math.atan2(-1,0)))
-	#print(math.sqrt(1**2+1**2))
-
-
+	for item in degrList:
+		item[0][0] += best[0]
+		item[0][1] += best[1]
+		tmpTuple = (item[0][0],item[0][1])
+		degrSet.append((item[0][0],item[0][1]))
+	sortList = sorted(degrSet, key=lambda degrSet: (90 + math.degrees(math.atan2(*tuple(map(operator.sub, degrSet, best2))[::-1]))) % 360)
+	print(sortList)
+	print((sortList[199][0])*100+sortList[199][1])
+	print(sortList[199])
 
 Part2()
